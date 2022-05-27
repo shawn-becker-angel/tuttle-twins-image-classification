164f51fdc585510f38269626bcd81ac0cbd03434
Shawn Becker <shawn.becker@angel.com>
2022-05-26 18:40:00 -0600

    dataset-from-csv-tf-metal works!!

--- dataset-from-csv-tf-metal.py
+++ dataset-from-csv-tf-metal.py
@@ -9,14 +9,32 @@
 # python3.8 -m pip install --upgrade pip
 # SYSTEM_VERSION_COMPAT=0 pip install tensorflow-macos tensorflow-metal
 # pip install tensorflow_datasets
-
-from sklearn.semi_supervised import LabelSpreading
-import tensorflow as tf
-# import tensorflow_datasets as tfds
-import pandas as pd
 import os
 
-# verify using GPU
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+import tensorflow as tf
+# import tensorflow_datasets as tfds
+
+# pip uninstall PIL
+# pip install Image
+
+import pandas as pd
+import numpy as np
+
+import os
+
+# see https://vijayabhaskar96.medium.com/tutorial-on-keras-flow-from-dataframe-1fd4493d237c
+# pip uninstall keras-preprocessing
+# pip install git+https://github.com/keras-team/keras-preprocessing.git
+# Import from keras_preprocessing not from keras.preprocessing
+
+from keras.models import Sequential
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers import Input, Dense, Activation, Flatten, Dropout, BatchNormalization
+from keras.layers import Conv2D, MaxPooling2D
+from keras import regularizers, optimizers
+
+# verify use of GPU
 tf.config.list_physical_devices()
 with tf.device('/GPU'):
     a = tf.random.normal(shape=(2,), dtype=tf.float32)
@@ -26,131 +44,124 @@ with tf.device('/GPU'):
 
 # Constants
 IMAGE_SIZE = 128
-NUM_CLASSES = 5
-BATCH_SIZE = 64
-EPOCHS = 100
-AUTOTUNE = tf.data.AUTOTUNE
+IMAGE_HEIGHT = IMAGE_SIZE
+IMAGE_WIDTH = IMAGE_SIZE
+
+# batch_size must evenly divide the length of the dataset exactly
+# because for the test set, you should sample the images exactly once
+BATCH_SIZE = 32
+EPOCHS = 50
 
 IMAGE_SOURCE_DIRECTORY = "../src-images/"
-if not os.path.isdir(IMAGE_SOURCE_DIRECTORY):
-    raise Exception(f"no such directory: {IMAGE_SOURCE_DIRECTORY}")
 
+train_df = pd.read_csv("../csv-data/train_data.csv",header=None, names=['filename','label'])
+test_df = pd.read_csv("../csv-data/test_data.csv",header=None, names=['filename','label'])
 
+# the number of samples used, N, must be 
+# divisible by 32, the test batch_size, it must also be
+# divisible  by 4, the validation split (but 32 is already a multiple of 4)\
 
-# helper functions
+orig_N = len(train_df)
+N = (orig_N // BATCH_SIZE) * BATCH_SIZE
+print("orig_N:", orig_N, "N:", N)
 
-def read_image(file_name, label):
-    image = tf.io.read_file(
-        IMAGE_SOURCE_DIRECTORY + file_name)
-    image = tf.image.decode_jpeg(
-        image, 
-        channels=3,
-        ratio=4)                # downscaling_ratio to get us close to final size
-    image = tf.image.resize(
-        image, size=(128, 128),
-        preserve_aspect_ratio=False, 
-        antialias=True)
-    return image, label
+# truncate the dataframes to N
+train_df = train_df[:N]
+test_df = test_df[:N]
 
-def augment_image(image, label):
-    return image, label
+datagen = ImageDataGenerator(rescale=1./255.,validation_split=0.25)
 
-def normalize_image(image, label):
-    image = tf.cast(image, tf.float32)
-    image = image / 255. # normalize
-    return image, label
+train_generator = datagen.flow_from_dataframe(
+    dataframe=train_df,
+    directory="../src-images/",
+    x_col="filename",
+    y_col="label",
+    subset="training",
+    batch_size=BATCH_SIZE,
+    seed=42,
+    shuffle=True,
+    drop_duplicates=False, # not needed
+    validate_filenames=False, # not needed
+    class_mode="categorical",
+    target_size=(IMAGE_HEIGHT,IMAGE_WIDTH))
 
-def get_dataset_partitions_tf(
-    ds, ds_size, train_split=0.8, test_split=0.1, pred_split=0.1, 
-    shuffle=True, shuffle_size=10000):
-    
-    assert (train_split + pred_split + test_split) == 1
-    
-    if shuffle:
-        # Specify seed to always have the same split distribution between runs
-        ds = ds.shuffle(shuffle_size, seed=12)
-    
-    train_size = int(train_split * ds_size)
-    val_size = int(test_split * ds_size)
-    
-    train_ds = ds.take(train_size)    
-    val_ds = ds.skip(train_size).take(val_size)
-    test_ds = ds.skip(train_size).skip(val_size)
-    
-    return train_ds, val_ds, test_ds
+NUM_CLASSES = len(train_generator.class_indices)
 
+labels_to_ints = train_generator.class_indices
+ints_to_labels = {y: x for x, y in labels_to_ints.items()}
 
-# read the train data file into train_df
-train_csv_file =  "../csv-data/train_data.csv"
-if not os.path.isfile(train_csv_file):
-    raise Exception(f"no such file: {train_csv_file}")
-train_df = pd.read_csv(train_csv_file,header=None, names=['file_name','label'])
-print("train_df.shape:", train_df.shape)
+valid_generator = datagen.flow_from_dataframe(
+    dataframe=train_df,
+    directory="../src-images/",
+    x_col="filename",
+    y_col="label",
+    subset="validation",
+    batch_size=BATCH_SIZE, 
+    seed=42,
+    shuffle=True,
+    drop_duplicates=False, # not needed
+    validate_filenames=False, # not needed
+    class_mode="categorical",
+    target_size=(IMAGE_HEIGHT,IMAGE_WIDTH))
 
-# read the test data file into test_df
-test_csv_file =  "../csv-data/test_data.csv"
-if not os.path.isfile(test_csv_file):
-    raise Exception(f"no such file: {test_csv_file}")
-test_df = pd.read_csv(test_csv_file,header=None, names=['file_name','label'])
-print("test_df.shape:", test_df.shape)
+test_datagen = ImageDataGenerator(rescale=1./255.)
 
-# combine train_df and test_df into a single df
-df = pd.concat([train_df,test_df],axis=0)
-print("df.shape:", df.shape)
+test_generator = test_datagen.flow_from_dataframe(
+    dataframe=test_df,
+    directory="../src-images/",
+    x_col="filename",
+    y_col=None, # images only
+    batch_size=BATCH_SIZE,
+    seed=42,
+    shuffle=False, # not needed for testing
+    drop_duplicates=False, # not needed
+    validate_filenames=False, # not needed
+    class_mode=None, # images only
+    target_size=(IMAGE_HEIGHT,IMAGE_WIDTH))
 
-# file_names and labels vectors
-file_names = df['file_name'].values
-labels = df['label'].values
-N = len(file_names)
-assert len(labels == N)
+model = Sequential()
+model.add(Conv2D(32, (3, 3), padding='same',
+                 input_shape=(IMAGE_HEIGHT,IMAGE_WIDTH,3)))
+model.add(Activation('relu'))
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
 
-# create ds_train, the train dataset
-ds_train = tf.data.Dataset.from_tensor_slices((file_names, labels))
+model.add(Conv2D(64, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
 
-ds_train = ds_train.map(read_image).map(augment_image).batch(16)
+model.add(Flatten())
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(NUM_CLASSES, activation='softmax'))
 
-# split out ds_test and ds_pred from ds_train
-(ds_train, ds_test, ds_pred) = get_dataset_partitions_tf(
-    ds_train, N, train_split=0.8, test_split=0.1, pred_split=0.1, 
-    shuffle=True, shuffle_size=10000)
+model.compile(optimizers.RMSprop(learning_rate=0.0001),
+loss="categorical_crossentropy", metrics=["accuracy"])
+STEP_SIZE_TRAIN=train_generator.n//train_generator.batch_size
+STEP_SIZE_VALID=valid_generator.n//valid_generator.batch_size
+model.fit_generator(
+    generator=train_generator,
+    steps_per_epoch=STEP_SIZE_TRAIN,
+    validation_data=valid_generator,
+    validation_steps=STEP_SIZE_VALID,
+    epochs=EPOCHS)
 
-# prep ds_train
-ds_train = ds_train.map(normalize_image, num_parallel_calls=AUTOTUNE)
-ds_train = ds_train.cache()
-# ds_train = ds_train.shuffle(ds_info.splits['train'].num_examples)
-ds_train = ds_train.batch(BATCH_SIZE)
-ds_train = ds_train.prefetch(AUTOTUNE)
+score = model.evaluate_generator(valid_generator)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
 
-# prep ds_test
-ds_test = ds_test.map(normalize_image, num_parallel_calls=AUTOTUNE)
-ds_test = ds_test.cache()
-ds_test = ds_test.batch(BATCH_SIZE)
+predict=model.predict_generator(test_generator, steps = len(test_generator.filenames))
 
-print('\ncreate the model')
-model = tf.keras.Sequential([
-    tf.keras.layers.Conv2D(16, 3, padding='same', activation='relu'),
-    tf.keras.layers.MaxPooling2D(),
-    tf.keras.layers.Conv2D(32, 3, padding='same', activation='relu'),
-    tf.keras.layers.MaxPooling2D(),
-    tf.keras.layers.Conv2D(64, 3, padding='same', activation='relu'),
-    tf.keras.layers.MaxPooling2D(),
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dense(NUM_CLASSES)
-])
+y_classes = predict.argmax(axis=-1)
+filenames = test_generator.filenames
+assert len(y_classes) == len(filenames)
 
-print('\ncompile the model')
-model.compile(
-    optimizer='rmsprop',
-    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-    metrics=['accuracy'])
-
-print("\nnow fit model to ds_train and ds_test")
-model.fit(ds_train, epochs=EPOCHS, validation_data=ds_test)
-
-
-# for epoch in range(EPOCHS):
-#     for x, y in ds_train:
-#         # train here
-#         pass
-
+for i in range(len(y_classes)):
+    label = ints_to_labels[y_classes[i]]
+    print(f"predicted label:{label} for filename[{i}]:{filenames[i]}")
